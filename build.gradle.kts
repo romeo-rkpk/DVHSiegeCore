d5e6b49c02a1d71a317b093b5f865bd7ce9cd9c3
RomeoRKPK <mindaye8504@gmail.com>
2022-12-28 07:02:58 +0900

    add dependent

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.21"
+    id("co.uzzu.dotenv.gradle") version "2.0.0"
 }
 
 group = "com.danvhae.minecraft.siege"
@@ -18,11 +19,19 @@ repositories {
         name = "spigotmc-repo"
         url = uri("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
     }
+
+    //mavenLocal()
 }
 
 dependencies {
     testImplementation(kotlin("test"))
     compileOnly("org.spigotmc:spigot-api:1.12.2-R0.1-SNAPSHOT")
+
+    compileOnly(files(env.fetch("WORLD_EDIT_FILE")))
+    compileOnly(files(env.fetch("WORLD_GUARD_FILE")))
+    compileOnly(files(env.fetch("DP_CORE_FILE")))
+    compileOnly(files(env.fetch("DP_RPG_FILE")))
+
 
 }
 
