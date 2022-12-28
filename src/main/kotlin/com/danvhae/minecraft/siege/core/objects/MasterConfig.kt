d9e095c3afbb9600b3a8fd52fbd71c9e7561149b
RomeoRKPK <mindaye8504@gmail.com>
2022-12-28 16:11:36 +0900

    Feat: 콘피그 제작됨

--- src/main/kotlin/com/danvhae/minecraft/siege/core/objects/MasterConfig.kt
+++ src/main/kotlin/com/danvhae/minecraft/siege/core/objects/MasterConfig.kt
@@ -1,4 +1,27 @@
 package com.danvhae.minecraft.siege.core.objects
 
-class MasterConfig(var period:Boolean, var sirius:Boolean, var wildWorldName:String) {
+import com.danvhae.minecraft.siege.core.utils.FileUtil
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+
+class MasterConfig(var period:Boolean = false, var sirius:Boolean = false, var wildWorldName:String = "world") {
+    companion object{
+        private const val FILE_NAME = "master.json"
+
+        fun load():MasterConfig{
+            return try{
+                val gson = Gson()
+                val json = FileUtil.readTextFile(FILE_NAME)
+                gson.fromJson(json, MasterConfig::class.java)
+            }catch (_:Exception){
+                MasterConfig()
+            }
+        }
+
+        fun save(config:MasterConfig){
+            val gson = GsonBuilder().setPrettyPrinting().create()
+            val json = gson.toJson(config)
+            FileUtil.writeTextFile(json, FILE_NAME)
+        }
+    }
 }
