fb1584bfd9f5b6ed786bf637a1815b96d8f93aed
RomeoRKPK <mindaye8504@gmail.com>
2022-12-29 18:38:23 +0900

    Feat: uuid name 유틸 재작성

--- src/main/kotlin/com/danvhae/minecraft/siege/core/utils/PlayerUtil.kt
+++ src/main/kotlin/com/danvhae/minecraft/siege/core/utils/PlayerUtil.kt
@@ -1,10 +1,19 @@
 package com.danvhae.minecraft.siege.core.utils
 
-import com.danvhae.minecraft.siege.core.objects.SiegeCastle
 import com.danvhae.minecraft.siege.core.DVHSiegeCore
+import com.danvhae.minecraft.siege.core.objects.SiegeCastle
+import com.google.gson.Gson
+import com.google.gson.JsonObject
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin
+import org.bukkit.Bukkit
 import org.bukkit.Location
 import org.bukkit.entity.Player
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import java.util.*
 
 class PlayerUtil {
     companion object{
@@ -35,6 +44,96 @@ class PlayerUtil {
                 if(castle.worldGuardID in idSet)return false
             }
             return true
+        }
+
+        private val uuidToNameCache = HashMap<UUID, String>()
+        private val nameToUUIDCache = HashMap<String, UUID>()
+
+        private fun httpRequest(urlString: String, parameter: String): String? {
+            return try {
+                val url = URL(urlString + parameter)
+                val connection = url.openConnection() as HttpURLConnection
+                connection.requestMethod = "GET"
+                val reader = BufferedReader(
+                    InputStreamReader(
+                        connection.inputStream
+                    )
+                )
+                var inputLine: String?
+                val stringBuilder = StringBuilder()
+                while (reader.readLine().also { inputLine = it } != null) {
+                    stringBuilder.append(inputLine)
+                }
+                reader.close()
+                stringBuilder.toString()
+            } catch (e: IOException) {
+                null
+            }
+        }
+
+        private fun addDashToUUIDString(uuidString: String): String {
+            return uuidString.replace(
+                "(\\w{8})(\\w{4})(\\w{4})(\\w{4})(\\w{12})".toRegex(),
+                "$1-$2-$3-$4-$5"
+            ).replace("\"".toRegex(), "")
+        }
+
+
+        fun nameToUUID(name: String?): UUID? {
+            name?:return null
+            val player = Bukkit.getPlayer(name)
+            player?.let {
+                nameToUUIDCache[player.name] = player.uniqueId
+                uuidToNameCache[player.uniqueId] = player.name
+                return player.uniqueId
+            }
+
+            val json = httpRequest("https://api.mojang.com/users/profiles/minecraft/", name)
+            val jsonObject = Gson().fromJson(json, JsonObject::class.java)
+            jsonObject["id"]?.let {
+                val uuid = UUID.fromString(addDashToUUIDString(it.toString()))
+                val getName = jsonObject["name"]!!.asString!!
+                uuidToNameCache[uuid] = getName
+                nameToUUIDCache[getName] = uuid
+                return uuid
+            }
+            return null
+            /*
+            if (name == null) return null
+            val player = Bukkit.getPlayer(name)
+            if (player != null) return player.uniqueId
+
+            var uuid: UUID? = nameToUUIDCache[name]
+            if (uuid != null) return uuid
+            val json = httpRequest("https://api.mojang.com/users/profiles/minecraft/", name)
+            val `object` = Gson().fromJson(json, JsonObject::class.java)
+            val element = `object`["id"] ?: return null
+            val uuidString = element.toString() ?: return null
+            val addHyphen = addDashToUUIDString(uuidString)
+            uuid = UUID.fromString(addHyphen)
+            val cachedName = Objects.requireNonNull(`object`["name"]).asString
+            nameToUUIDCache.put(cachedName ?: name, uuid)
+            uuidToNameCache.put(uuid, cachedName ?: name)
+            return uuid
+
+             */
+        }
+
+        fun uuidToName(uuid: UUID?): String? {
+            if (uuid == null) return null
+            val player = Bukkit.getPlayer(uuid)
+            if (player != null) return player.name
+            uuidToNameCache[uuid]?.let { return it }
+            val json = httpRequest("https://sessionserver.mojang.com/session/minecraft/profile/", uuid.toString())
+            val `object` = Gson().fromJson(json, JsonObject::class.java)
+            val element = `object`["name"] ?: return null
+            //result = element.asString
+            //if (result == null) return null
+            element.asString?.let {
+                nameToUUIDCache[it] = uuid
+                uuidToNameCache[uuid] = it
+                return it
+            }?:return null
         }
     }
 }
