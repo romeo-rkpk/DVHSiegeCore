9dfd545cbf4ce200e4692ac39ac5413e2ecc310f
RomeoRKPK <mindaye8504@gmail.com>
2022-12-29 22:28:40 +0900

    Fix: 팀 변경시 저장하도록 개선

--- src/main/kotlin/com/danvhae/minecraft/siege/core/objects/SiegePlayer.kt
+++ src/main/kotlin/com/danvhae/minecraft/siege/core/objects/SiegePlayer.kt
@@ -24,6 +24,7 @@ class SiegePlayer(val playerUUID:UUID, team: String, isOwner:Boolean, val alias:
                     }
                 }
             }
+            save()
             //scoreTeam.removeEntry()
         }
 
