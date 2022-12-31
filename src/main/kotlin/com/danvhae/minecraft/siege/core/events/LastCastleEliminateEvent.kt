238c13458dd7effa785760e438d2be1351a33bcc
RomeoRKPK <mindaye8504@gmail.com>
2023-01-01 04:24:24 +0900

    Fix: 패키지 정리

--- src/main/kotlin/com/danvhae/minecraft/siege/core/events/LastCastleEliminateEvent.kt
+++ src/main/kotlin/com/danvhae/minecraft/siege/core/events/LastCastleEliminateEvent.kt
@@ -1,4 +1,4 @@
-package com.danvhae.minecraft.siege.core.commands
+package com.danvhae.minecraft.siege.core.events
 
 import com.danvhae.minecraft.siege.core.abstracts.KotlinBukkitEventAbstract
 import com.danvhae.minecraft.siege.core.objects.SiegeTeam
