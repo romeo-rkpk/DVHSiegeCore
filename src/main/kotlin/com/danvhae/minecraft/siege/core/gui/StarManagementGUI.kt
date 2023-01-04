26090f2dbe90f6586b4ebf21fe5dd2e0e2638fcf
RomeoRKPK <mindaye8504@gmail.com>
2023-01-05 00:52:43 +0900

    Feat: 메인 GUI 작성

---
+++ src/main/kotlin/com/danvhae/minecraft/siege/core/gui/StarManagementGUI.kt
@@ -0,0 +1,51 @@
+package com.danvhae.minecraft.siege.core.gui
+
+import com.danvhae.minecraft.siege.core.objects.DVHStaticGUI
+import com.danvhae.minecraft.siege.core.objects.SiegeCastle
+import com.danvhae.minecraft.siege.core.objects.SiegePlayer
+import com.danvhae.minecraft.siege.core.utils.TextUtil
+import org.bukkit.ChatColor
+import org.bukkit.Material
+import org.bukkit.inventory.Inventory
+import org.bukkit.inventory.ItemStack
+
+class StarManagementGUI(val castle:SiegeCastle, val siegePlayer: SiegePlayer) {
+
+    companion object{
+        const val id = "별 관리"
+        const val ROWS = 3
+
+
+        const val STAR_ICON_SLOT = 4
+
+
+        fun parse(inventory: Inventory, player:SiegePlayer):StarManagementGUI?{
+
+            for(castle in SiegeCastle.DATA.values){
+                if(ChatColor.stripColor(inventory.getItem(StarBuyConfirmGUI.STAR_ICON_POSITION).itemMeta.displayName) == castle.name){
+                    return StarManagementGUI(castle, player)
+                }
+            }
+
+            return null
+        }
+    }
+
+    fun gui():Inventory{
+        val inventory = DVHStaticGUI[id]!!.createInventory()
+
+        //var stack: ItemStack = ItemStack(Material.STAINED_GLASS_PANE, 1, 0)
+        for(i in 0 until inventory.size)
+            inventory.setItem(i, ItemStack(Material.STAINED_GLASS_PANE, 1, 0)) //white glass pane
+
+        for(button in DVHStaticGUI[StarShopGUI.id]!!.buttons){
+            if(ChatColor.stripColor(TextUtil.toColor(button.name)) != castle.name)continue
+            inventory.setItem(STAR_ICON_SLOT, button.toItemStack())
+            break
+        }
+
+        return inventory
+
+    }
+
+}
