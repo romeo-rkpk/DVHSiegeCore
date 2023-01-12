6c7479f9bf3b17d298ff54b88396b0aeb16e3977
RomeoRKPK <mindaye8504@gmail.com>
2023-01-12 15:29:14 +0900

    Fix: 현재 달성한 레벨 표시 수정

--- src/main/kotlin/com/danvhae/minecraft/siege/core/gui/StarLevelUpConfirmGUI.kt
+++ src/main/kotlin/com/danvhae/minecraft/siege/core/gui/StarLevelUpConfirmGUI.kt
@@ -1,9 +1,11 @@
 package com.danvhae.minecraft.siege.core.gui
 
 import com.danvhae.minecraft.siege.core.abstracts.WhiteBackgroundStarGUI
+import com.danvhae.minecraft.siege.core.objects.DVHStaticGUI
 import com.danvhae.minecraft.siege.core.objects.SiegeCastle
 import com.danvhae.minecraft.siege.core.objects.SiegePlayer
 import com.danvhae.minecraft.siege.core.utils.TextUtil
+import org.bukkit.ChatColor
 import org.bukkit.inventory.Inventory
 
 class StarLevelUpConfirmGUI(override val castle:SiegeCastle, val siegePlayer: SiegePlayer)
@@ -25,6 +27,28 @@ class StarLevelUpConfirmGUI(override val castle:SiegeCastle, val siegePlayer: Si
         meta.lore = prev.toList()
         icon.itemMeta = meta
         inventory.setItem(starIconPosition, icon)
+
+        /*
+        val meta = event.currentItem.itemMeta?:return
+        val displayName = ChatColor.stripColor(meta.displayName)?:return
+        val (requestedStr) = regex.find(displayName)!!.destructured
+
+        //Bukkit.getLogger().warning(requestedStr)
+        val requestedLevel = requestedStr.toIntOrNull()?:return
+         */
+        for(button in DVHStaticGUI[id]!!.buttons){
+            val stack = button.toItemStack()
+            val meta1 = stack.itemMeta?:continue
+            val displayName = ChatColor.stripColor(meta1.displayName)?:continue
+            val (level) = Regex("(\\d+)레벨").find(displayName)?.destructured?:continue
+            meta1.lore = listOf(TextUtil.toColor(when(level.toInt()){
+                in 0 .. castle.level -> "&f이미 달성한 레벨입니다"
+                castle.level + 1 -> "&f신청 가능한 레벨입니다."
+                else -> "&c지금은 신청할 수 없는 레벨입니다."
+            }))
+            stack.itemMeta = meta1
+            inventory.setItem(button.slot, stack)
+        }
         return inventory
     }
 
