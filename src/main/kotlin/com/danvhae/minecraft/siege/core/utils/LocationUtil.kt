f84379e7fc9fb45f1a420cf85a8f6a64384906c9
RomeoRKPK <mindaye8504@gmail.com>
2023-01-14 01:36:10 +0900

    Feat: 함수 작성

--- src/main/kotlin/com/danvhae/minecraft/siege/core/utils/LocationUtil.kt
+++ src/main/kotlin/com/danvhae/minecraft/siege/core/utils/LocationUtil.kt
@@ -1,8 +1,11 @@
 package com.danvhae.minecraft.siege.core.utils
 
+import com.danvhae.minecraft.siege.core.enums.SiegeCastleStatus
 import com.danvhae.minecraft.siege.core.objects.SiegeCastle
+import com.danvhae.minecraft.siege.core.objects.SiegePlayer
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin
 import org.bukkit.Location
+import org.bukkit.entity.Player
 
 class LocationUtil {
     companion object{
@@ -25,6 +28,20 @@ class LocationUtil {
                     result.add(castle)
             }
             return result
+        }
+
+        /**
+         * 이 플레이어가 공성 중인지 판단합니다
+         */
+        fun attacking(player: Player):Boolean{
+            val sPlayer = SiegePlayer[player.uniqueId]?:return false
+            for(castle in locationAtStars(player.location)){
+                if(castle.status !in listOf(SiegeCastleStatus.UNDER_BATTLE, SiegeCastleStatus.PEACEFUL))continue
+                if(castle.team != sPlayer.team){
+                    return true
+                }
+            }
+            return false
         }
     }
 }
